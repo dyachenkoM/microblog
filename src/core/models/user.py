be8b841dd5c4fc8b167ee281022bdd26b7da555d
Maksim <kekswat@mail.ru>
2025-04-07 14:51:33 +0700

    feat: refactor user model

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,11 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from core.models.db_helper import Base
+from .base import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     followers = Column(Integer, default=0)
     following = Column(Integer)
