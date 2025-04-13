22bfac78a5986f77fc5983bf17f329cb82872530
Maksim <kekswat@mail.ru>
2025-04-13 23:06:19 +0700

    feat: refactor user model

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,10 +1,34 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship, foreign
+from sqlalchemy import String, Integer, ForeignKey
+from typing import List
 from .base import Base
+
+
+class FollowersTable(Base):
+    __tablename__ = "followers"
+
+    follower_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), primary_key=True)
+    following_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), primary_key=True)
 
 
 class User(Base):
     __tablename__ = "users"
 
-    name = Column(String, index=True)
-    followers = Column(Integer, default=0)
-    following = Column(Integer)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, index=True)
+    followers: Mapped[List["User"]] = relationship(
+        secondary=FollowersTable.__table__,
+        primaryjoin=id == foreign(FollowersTable.following_id),
+        secondaryjoin=id == foreign(FollowersTable.follower_id),
+        back_populates="following",
+        lazy="joined",
+    )
+
+    following: Mapped[List["User"]] = relationship(
+        secondary=FollowersTable.__table__,
+        primaryjoin=id == foreign(FollowersTable.follower_id),
+        secondaryjoin=id == foreign(FollowersTable.following_id),
+        back_populates="followers",
+        lazy="joined",
+    )
+
