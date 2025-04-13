c003df2c74f21aba4fcb91d0d4376333feb1e424
Maksim <kekswat@mail.ru>
2025-04-13 23:03:36 +0700

    feat: Add route get user by id

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 import uvicorn
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import joinedload
+from core.models import User
 from contextlib import asynccontextmanager
 from api import router as api_router
 from core.config import settings
-from core.models import db_helper
+from core.models import db_helper, Base
+from core.schemas import UserResponse, UserFull
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     yield
-    print("engine dispose")
     await db_helper.dispose()
 
 
@@ -33,6 +39,24 @@ async def main():
             "followings": []
         }
     }
+
+
+@main_app.get('/api/users/{user_id}')
+async def get_user(user_id: int, session: AsyncSession = Depends(db_helper.session_getter)) -> UserResponse:
+    result = await session.execute(
+        select(User)
+        .options(
+            joinedload(User.followers),
+            joinedload(User.following),
+        )
+        .where(User.id == user_id)
+    )
+    user = result.unique().scalar_one()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return UserResponse(result='true', user=UserFull.model_validate(user))
 
 
 if __name__ == "__main__":
