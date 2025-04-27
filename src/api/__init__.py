be1206e2ef51f6467e6a91b7b078e77d2b2c1a6d
Maksim <kekswat@mail.ru>
2025-04-27 16:53:53 +0700

    feat: add user router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,3 +1,12 @@
 from fastapi import APIRouter
+from core.config import settings
+from .user import router as user_router
 
-router = APIRouter()
+
+router = APIRouter(
+    prefix=settings.api.prefix,
+)
+
+router.include_router(
+    user_router,
+)
