e9df899b9373707a982a0afcabf61f5b51ac4fe9
Maksim <kekswat@mail.ru>
2025-04-27 17:32:18 +0700

    feat: add FollowersTable to __all__

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "FollowersTable",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+from .user import FollowersTable
