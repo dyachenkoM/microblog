61a99c164f09221b013c9b15011066359cb98175
Maksim <kekswat@mail.ru>
2025-05-05 15:18:48 +0700

    feat: add user-key to __all__

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "Base",
     "User",
     "FollowersTable",
+    "UserKey"
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
 from .user import FollowersTable
+from .user import UserKey
