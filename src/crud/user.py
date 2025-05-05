2c4135ce95cd73de7493a8981018c28b2badc816
Maksim <kekswat@mail.ru>
2025-05-05 15:19:36 +0700

    feat: add get user by api-key and follow user func

--- src/crud/user.py
+++ src/crud/user.py
@@ -1,10 +1,10 @@
 from typing import Sequence
 
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from core.models import User
+from core.models import User, FollowersTable, UserKey
 
 
 async def get_all_users(session: AsyncSession) -> Sequence[User]:
@@ -23,3 +23,18 @@ async def get_user_by_id(session: AsyncSession, user_id: int) -> User:
     )
     result = await session.scalar(stmt)
     return result
+
+
+async def get_user_id_by_api_key(session: AsyncSession, api_key: str) -> int:
+    stmt = select(UserKey).where(UserKey.api_key == api_key)
+
+    result = await session.scalar(stmt)
+    return result.user_id
+
+
+async def follow_user(session: AsyncSession, follower_id: int, following_id: int) -> dict:
+    new_follow = FollowersTable(follower_id=follower_id, following_id=following_id)
+    session.add(new_follow)
+    await session.commit()
+
+    return {"result": "true"}
