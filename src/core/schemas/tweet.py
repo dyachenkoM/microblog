fb07aa066a615a2bdb49269fe1eb08fad3ab760f
Maksim <kekswat@mail.ru>
2025-05-07 23:28:16 +0700

    feat: add tweet schema

---
+++ src/core/schemas/tweet.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from user import UserShort
+
+
+class Tweet(BaseModel):
+    id: int
+    content: str
+    attachments: list[str]
+    author: UserShort
+    likes: list[UserShort]
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class TweetResponse(BaseModel):
+    result: bool
+    tweets: list[Tweet]
