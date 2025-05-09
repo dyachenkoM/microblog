7a40197721b5945763af43db7c6e4a054464da26
Maksim <kekswat@mail.ru>
2025-05-09 16:25:48 +0700

    feat: add cascade behaviour and like count func

--- src/core/models/tweet.py
+++ src/core/models/tweet.py
@@ -1,7 +1,10 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from typing_extensions import cast
+
 from .association_tables import TweetAttachment, TweetLikes
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import Integer, String, ForeignKey, select, func
 from typing import List
 
 
@@ -17,6 +20,7 @@ class Tweet(Base):
         secondary=TweetAttachment.__table__,
         back_populates="tweets",
         lazy="joined",
+        cascade="all, delete"
     )
     likes: Mapped[List["User"]] = relationship(
         secondary=TweetLikes.__table__,
@@ -24,6 +28,19 @@ class Tweet(Base):
         lazy="joined",
     )
 
+    @hybrid_property
+    def likes_count(self) -> int:
+        likes = cast(List[User], self.likes)
+        return len(likes) if likes else 0
+
+    @likes_count.expression
+    def likes_count(self):
+        cls = self.__class__
+        return (
+            select(func.count(TweetLikes.user_id))
+            .where(TweetLikes.tweet_id == cls.id)
+            .scalar_subquery()
+        )
 
 from .user import User  # noqa
 from .attachment import Attachment # noqa
