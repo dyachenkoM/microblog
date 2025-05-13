4367b7883d9de46242d8a2508109192d42e43960
Maksim <kekswat@mail.ru>
2025-05-13 20:33:41 +0700

    feat: add crud to attachment

---
+++ src/crud/attachment.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models.attachment import Attachment
+
+
+async def create_attachment(
+        session: AsyncSession,
+        url: str,
+) -> Attachment:
+
+    new_attachment = Attachment(
+        link=url
+    )
+
+    session.add(new_attachment)
+    await session.commit()
+
+    return new_attachment.id
