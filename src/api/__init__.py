89c9817a300be684b9cc6e071be442c4a1e1a2fe
Maksim <kekswat@mail.ru>
2025-05-13 20:36:30 +0700

    feat: add media router to __all__

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from core.config import settings
 from .user import router as user_router
 from .tweet import router as tweet_router
+from .media import router as media_router
 
 
 router = APIRouter(
@@ -10,3 +11,4 @@ router = APIRouter(
 
 router.include_router(user_router)
 router.include_router(tweet_router)
+router.include_router(media_router)
