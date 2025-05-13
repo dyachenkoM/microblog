a9fed40f25d887fbcef1e27ea624e25b7efe9cc3
Maksim <kekswat@mail.ru>
2025-05-13 21:03:04 +0700

    feat: add create tweet endpoint

--- src/api/tweet.py
+++ src/api/tweet.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, Header
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import db_helper
-from core.schemas import TweetResponse
+from core.schemas import TweetResponse, TweetCreateRequest, TweetCreateResponse
 from crud.tweet import (get_all_tweets as crud_get_all_tweets,
                         like_tweet as crud_like_tweet,
-                        dislike_tweet as crud_dislike_tweet)
+                        dislike_tweet as crud_dislike_tweet,
+                        create_tweet as crud_create_tweet)
 
 
 router = APIRouter(
@@ -20,6 +21,19 @@ async def get_all_tweets(session: AsyncSession = Depends(db_helper.session_gette
                          ) -> TweetResponse:
     tweets = await crud_get_all_tweets(session=session, api_key=api_key)
     return TweetResponse(result=True, tweets=tweets)
+
+
+@router.post("")
+async def create_tweet(tweet_data: TweetCreateRequest,
+                       session: AsyncSession = Depends(db_helper.session_getter),
+                       api_key: str | None = Header(default="api-alice"),
+                       ) -> TweetCreateResponse:
+    tweet = await crud_create_tweet(session=session,
+                                    api_key=api_key,
+                                    tweet_data=tweet_data.tweet_data,
+                                    tweet_media_ids=tweet_data.tweet_media_ids
+                                    )
+    return TweetCreateResponse(result=True, tweet_id=tweet.id)
 
 
 @router.post("/{tweet_id}/likes")
