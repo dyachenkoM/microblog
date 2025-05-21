268b8e55454400aa6ed1c619fa05baa5af8d1149
Maksim <kekswat@mail.ru>
2025-05-21 17:20:43 +0700

    feat: add media exception handlers

--- src/api/media.py
+++ src/api/media.py
@@ -1,8 +1,12 @@
+import logging
+
 from fastapi import APIRouter, UploadFile, Depends, Header
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import db_helper
+from core.exception import handle_error
 from core.s3 import s3_client
+from core.schemas import ErrorResponse
 from core.schemas.attachment import AttachmentResponse
 from crud.attachment import create_attachment
 
@@ -11,12 +15,18 @@ router = APIRouter(
     tags=["medias"]
 )
 
+logger = logging.getLogger("route_media")
 
-@router.post("")
+
+@router.post("", response_model=AttachmentResponse)
 async def file_upload(file: UploadFile,
                       session: AsyncSession = Depends(db_helper.session_getter),
                       api_key: str | None = Header(default="test")
-                      ):
-    media_url = await s3_client.upload_file(file.file, file.filename)
-    media_id = (await create_attachment(session=session, url=media_url)).id
-    return AttachmentResponse(result=True, media_id=media_id)
+                      ) -> AttachmentResponse | ErrorResponse:
+    try:
+        media_url = await s3_client.upload_file(file.file, file.filename)
+        media_id = (await create_attachment(session=session, url=media_url)).id
+        return AttachmentResponse(result=True, media_id=media_id)
+
+    except Exception as e:
+        return handle_error(e, logger)
--- src/crud/attachment.py
+++ src/crud/attachment.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models.attachment import Attachment
@@ -6,13 +7,17 @@ from core.models.attachment import Attachment
 async def create_attachment(
         session: AsyncSession,
         url: str,
-) -> Attachment:
+        ) -> Attachment:
+    try:
 
-    new_attachment = Attachment(
-        link=url
-    )
+        new_attachment = Attachment(
+            link=url
+        )
 
-    session.add(new_attachment)
-    await session.commit()
+        session.add(new_attachment)
+        await session.commit()
 
-    return new_attachment
+        return new_attachment
+
+    except SQLAlchemyError as e:
+        raise Exception(f"Database error: {str(e)}")
