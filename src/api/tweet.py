01371b55e28d2b42435cb55f5ca512bcd0a0edf8
Maksim <kekswat@mail.ru>
2025-05-21 17:09:18 +0700

    feat: add tweet exceptions handlers

--- src/api/tweet.py
+++ src/api/tweet.py
@@ -1,8 +1,18 @@
+import logging
+
 from fastapi import APIRouter, Depends, Header
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import db_helper
-from core.schemas import TweetResponse, TweetCreateRequest, TweetCreateResponse
+from core.schemas import (TweetResponse,
+                          TweetCreateRequest,
+                          TweetCreateResponse,
+                          SuccessResponse,
+                          ErrorResponse)
+from core.exception import (TweetNotFoundError,
+                            handle_error,
+                            UserNotFoundError)
+from core.models import Tweet
 from crud.tweet import (get_all_tweets as crud_get_all_tweets,
                         like_tweet as crud_like_tweet,
                         dislike_tweet as crud_dislike_tweet,
@@ -14,39 +24,67 @@ router = APIRouter(
     tags=["Tweets"]
 )
 
+logger = logging.getLogger("route_tweet")
+
 
 @router.get("")
 async def get_all_tweets(session: AsyncSession = Depends(db_helper.session_getter),
                          api_key: str | None = Header(default="test")
-                         ) -> TweetResponse:
-    tweets = await crud_get_all_tweets(session=session, api_key=api_key)
-    return TweetResponse(result=True, tweets=tweets)
+                         ) -> TweetResponse | ErrorResponse:
+    if not api_key:
+        raise UserNotFoundError("API key is required")
+    try:
+        tweets = await crud_get_all_tweets(session=session, api_key=api_key)
+        return TweetResponse(result=True, tweets=tweets)
+    except Exception as e:
+        return handle_error(e, logger)
 
 
 @router.post("")
 async def create_tweet(tweet_data: TweetCreateRequest,
                        session: AsyncSession = Depends(db_helper.session_getter),
                        api_key: str | None = Header(default="test"),
-                       ) -> TweetCreateResponse:
-    tweet = await crud_create_tweet(session=session,
-                                    api_key=api_key,
-                                    tweet_data=tweet_data.tweet_data,
-                                    tweet_media_ids=tweet_data.tweet_media_ids
-                                    )
-    return TweetCreateResponse(result=True, tweet_id=tweet.id)
+                       ) -> TweetCreateResponse | ErrorResponse:
+    if not api_key:
+        raise UserNotFoundError("API key is required")
+    try:
+        tweet = await crud_create_tweet(session=session,
+                                        api_key=api_key,
+                                        tweet_data=tweet_data.tweet_data,
+                                        tweet_media_ids=tweet_data.tweet_media_ids
+                                        )
+        return TweetCreateResponse(result=True, tweet_id=tweet.id)
+    except Exception as e:
+        return handle_error(e, logger)
 
 
 @router.post("/{tweet_id}/likes")
 async def like_tweet(tweet_id: int,
                      session: AsyncSession = Depends(db_helper.session_getter),
                      api_key: str | None = Header(default="test")
-                     ):
-    return await crud_like_tweet(session=session, tweet_id=tweet_id, api_key=api_key)
+                     ) -> SuccessResponse | ErrorResponse:
+    if not api_key:
+        raise UserNotFoundError("API key is required")
+    try:
+        tweet = await session.get(Tweet, tweet_id)
+        if not tweet:
+            raise TweetNotFoundError("Tweet not found")
+        return await crud_like_tweet(session=session, tweet_id=tweet_id, api_key=api_key)
+    except Exception as e:
+        return handle_error(e, logger)
 
 
 @router.delete("/{tweet_id}/likes")
 async def dislike_tweet(tweet_id: int,
                         session: AsyncSession = Depends(db_helper.session_getter),
                         api_key: str | None = Header(default="test")
-                        ):
-    return await crud_dislike_tweet(session=session, tweet_id=tweet_id, api_key=api_key)
+                        ) -> SuccessResponse | ErrorResponse:
+    if not api_key:
+        raise UserNotFoundError("API key is required")
+    try:
+        tweet = await session.get(Tweet, tweet_id)
+        if not tweet:
+            raise TweetNotFoundError("Tweet not found")
+        return await crud_dislike_tweet(session=session, tweet_id=tweet_id, api_key=api_key)
+    except Exception as e:
+        return handle_error(e, logger)
--- src/crud/tweet.py
+++ src/crud/tweet.py
@@ -1,53 +1,61 @@
 from typing import Optional, List
 
 from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
+from core.exception import APIError
 from core.models import Tweet, Attachment, TweetLikes
 from crud.user import get_user_by_api_key
+from core.schemas import SuccessResponse
 
 
-async def get_all_tweets(session: AsyncSession, api_key: str) -> [Tweet, ]:
-    user = await get_user_by_api_key(session=session, api_key=api_key)
+async def get_all_tweets(session: AsyncSession,
+                         api_key: str
+                         ) -> [Tweet, ]:
+    try:
+        user = await get_user_by_api_key(session=session, api_key=api_key)
+        following_ids = [u.id for u in user.following]
 
-    following_ids = [u.id for u in user.following]
-
-    stmt = (
-        select(Tweet)
-        .where(Tweet.author_id.in_(following_ids))
-        .options(
-            joinedload(Tweet.author),
-            joinedload(Tweet.attachments),
-            joinedload(Tweet.likes)
+        stmt = (
+            select(Tweet)
+            .where(Tweet.author_id.in_(following_ids))
+            .options(
+                joinedload(Tweet.author),
+                joinedload(Tweet.attachments),
+                joinedload(Tweet.likes)
+            )
+            .order_by(Tweet.likes_count.desc())
         )
-        .order_by(Tweet.likes_count.desc())
-    )
 
-    result = await session.execute(stmt)
-    tweets = result.unique().scalars().all()
+        result = await session.execute(stmt)
+        tweets = result.unique().scalars().all()
+        formatted_tweets = []
 
-    formatted_tweets = []
-    for tweet in tweets:
-        formatted_tweet = {
-            "id": tweet.id,
-            "content": tweet.content,
-            "attachments": [attachment.link for attachment in tweet.attachments],
-            "author": {
-                "id": tweet.author.id,
-                "name": tweet.author.name
-            },
-            "likes": [
-                {
-                    "user_id": user.id,
-                    "name": user.name
-                }
-                for user in tweet.likes
-            ]
-        }
-        formatted_tweets.append(formatted_tweet)
+        for tweet in tweets:
+            formatted_tweet = {
+                "id": tweet.id,
+                "content": tweet.content,
+                "attachments": [attachment.link for attachment in tweet.attachments],
+                "author": {
+                    "id": tweet.author.id,
+                    "name": tweet.author.name
+                },
+                "likes": [
+                    {
+                        "user_id": user.id,
+                        "name": user.name
+                    }
+                    for user in tweet.likes
+                ]
+            }
+            formatted_tweets.append(formatted_tweet)
 
-    return formatted_tweets
+        return formatted_tweets
+
+    except SQLAlchemyError as e:
+        raise APIError(f"Database error: {str(e)}")
 
 
 async def create_tweet(
@@ -55,55 +63,95 @@ async def create_tweet(
         api_key: str,
         tweet_data: str,
         tweet_media_ids: Optional[List[int]] = None
-) -> Tweet:
+        ) -> Tweet:
+    try:
+        stmt = select(Attachment).where(Attachment.id.in_(tweet_media_ids))
+        result = await session.execute(stmt)
+        existing_media = result.scalars().unique().all()
+        author_id = (await get_user_by_api_key(session=session, api_key=api_key)).id
 
-    stmt = select(Attachment).where(Attachment.id.in_(tweet_media_ids))
-    result = await session.execute(stmt)
-    existing_media = result.scalars().unique().all()
+        new_tweet = Tweet(
+            content=tweet_data,
+            author_id=author_id,
+            attachments=existing_media
+        )
+        session.add(new_tweet)
+        await session.commit()
+        await session.refresh(new_tweet)
 
-    new_tweet = Tweet(
-        content=tweet_data,
-        author_id=(await get_user_by_api_key(session=session, api_key=api_key)).id,
-        attachments=existing_media
-    )
+        return new_tweet
 
-    session.add(new_tweet)
-    await session.commit()
-    await session.refresh(new_tweet)
+    except IntegrityError as e:
+        await session.rollback()
+        raise Exception(f"Database integrity error: {str(e)}")
 
-    return new_tweet
+    except SQLAlchemyError as e:
+        raise APIError(f"Database error: {str(e)}")
 
 
-async def delete_tweet(session: AsyncSession, tweet_id: int, api_key: str):
-    user_id = (await get_user_by_api_key(session=session, api_key=api_key)).id
-    stmt = select(Tweet).where(Tweet.id == tweet_id)
+async def delete_tweet(session: AsyncSession,
+                       tweet_id: int,
+                       api_key: str
+                       ) -> SuccessResponse:
+    try:
+        user_id = (await get_user_by_api_key(session=session, api_key=api_key)).id
+        stmt = select(Tweet).where(Tweet.id == tweet_id)
+        tweet = await session.scalar(stmt)
 
-    tweet = await session.scalar(stmt)
-    if user_id == tweet.author_id:
-        await session.delete(tweet)
+        if user_id == tweet.author_id:
+            await session.delete(tweet)
+            await session.commit()
+
+            return SuccessResponse(result=True)
+
+    except IntegrityError as e:
+        await session.rollback()
+        raise Exception(f"Database integrity error: {str(e)}")
+
+    except SQLAlchemyError as e:
+        raise APIError(f"Database error: {str(e)}")
+
+
+async def like_tweet(session: AsyncSession,
+                     tweet_id: int,
+                     api_key: str
+                     ) -> SuccessResponse:
+    try:
+        user_id = (await get_user_by_api_key(session=session, api_key=api_key)).id
+        new_like = TweetLikes(user_id=user_id, tweet_id=tweet_id)
+
+        session.add(new_like)
         await session.commit()
 
-        return {"result": "true"}
+        return SuccessResponse(result=True)
+
+    except IntegrityError as e:
+        await session.rollback()
+        raise Exception(f"Database integrity error: {str(e)}")
+
+    except SQLAlchemyError as e:
+        raise APIError(f"Database error: {str(e)}")
 
 
-async def like_tweet(session: AsyncSession, tweet_id: int, api_key: str):
-    user_id = (await get_user_by_api_key(session=session, api_key=api_key)).id
-    new_like = TweetLikes(user_id=user_id, tweet_id=tweet_id)
+async def dislike_tweet(session: AsyncSession,
+                        tweet_id: int,
+                        api_key: str
+                        ) -> SuccessResponse:
+    try:
+        user_id = (await get_user_by_api_key(session=session, api_key=api_key)).id
+        stmt = select(TweetLikes).where(TweetLikes.tweet_id == tweet_id,
+                                        TweetLikes.user_id == user_id
+                                        )
+        like_relation = await session.scalar(stmt)
 
-    session.add(new_like)
-    await session.commit()
+        await session.delete(like_relation)
+        await session.commit()
 
-    return {"result": "true"}
+        return SuccessResponse(result=True)
 
+    except IntegrityError as e:
+        await session.rollback()
+        raise Exception(f"Database integrity error: {str(e)}")
 
-async def dislike_tweet(session: AsyncSession, tweet_id: int, api_key: str):
-    user_id = (await get_user_by_api_key(session=session, api_key=api_key)).id
-    stmt = select(TweetLikes).where(TweetLikes.tweet_id == tweet_id,
-                                    TweetLikes.user_id == user_id
-                                    )
-    like_relation = await session.scalar(stmt)
-
-    await session.delete(like_relation)
-    await session.commit()
-
-    return {"result": "true"}
+    except SQLAlchemyError as e:
+        raise APIError(f"Database error: {str(e)}")
