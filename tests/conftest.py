4df1e466240781e8a01955e9eebde127ebf8c092
Maksim <kekswat@mail.ru>
2025-06-06 20:19:13 +0700

    feat: refactor conftest to delete data only in the end of tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy import text
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     AsyncSession,
@@ -12,14 +11,44 @@ from core import db_helper
 from core.models import Base, User, UserKey
 from main import main_app as test_app
 
-
 TEST_DB_URL = "postgresql+asyncpg://user:password@localhost:5432/test_db"
-test_engine = create_async_engine(TEST_DB_URL, poolclass=NullPool)
+test_engine = create_async_engine(TEST_DB_URL, poolclass=NullPool, echo=True)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def db_session():
+    """Фикстура для управления жизненным циклом БД"""
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="session")
+async def prepare_user():
+    """Создаем тестового пользователя один раз для всех тестов"""
+    async_session = async_sessionmaker(
+        bind=test_engine,
+        expire_on_commit=False,
+        class_=AsyncSession,
+    )
+    async with async_session() as session:
+        user = User(name="TestUser")
+        session.add(user)
+        await session.flush()
+
+        key = UserKey(api_key="test", user_id=user.id)
+        session.add(key)
+        await session.commit()
+        yield
 
 
 @pytest.fixture(scope="function")
-async def client():
-    """Фикстура клиента FastAPI, с переопределённой зависимостью сессии"""
+async def client(db_session, prepare_user):
+    """Фикстура клиента с переиспользованием данных между тестами"""
 
     async def override_session():
         async_session = async_sessionmaker(
@@ -28,21 +57,7 @@ async def client():
             class_=AsyncSession,
         )
         async with async_session() as session:
-            await session.execute(text("TRUNCATE users_keys, users RESTART IDENTITY CASCADE"))
-            await session.commit()
-
-            user = User(name="TestUser")
-            session.add(user)
-            await session.flush()
-
-            key = UserKey(api_key="test", user_id=user.id)
-            session.add(key)
-
-            await session.commit()
             yield session
-
-    async with test_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
     test_app.dependency_overrides[db_helper.session_getter] = override_session
 
